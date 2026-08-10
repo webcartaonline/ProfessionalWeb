763b13f85fb5eafa471814a3c9a583641df9296d
webcartaonline <webcartaonline@gmail.com>
2026-08-11 00:29:44 +0200

    Reorganización

--- js/main.js
+++
@@ -1,8 +0,0 @@
-/**
- * Punto de entrada de JavaScript.
- * Registra los componentes personalizados usados en todo el sitio.
- */
-
-import "./components/site-navbar.js";
-import "./components/site-footer.js";
-import "./components/logo-marquee.js";
