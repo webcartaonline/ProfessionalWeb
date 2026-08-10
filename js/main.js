d1faf0633471ccf0c143900e5e47eda57df64c51
webcartaonline <webcartaonline@gmail.com>
2026-08-10 21:38:21 +0200

    fix: all links

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@
  * Registra los componentes personalizados usados en todo el sitio.
  */
 
-import "./components/site-navbar.js";
-import "./components/site-footer.js";
-import "./components/logo-marquee.js";
+import "components/site-navbar.js";
+import "components/site-footer.js";
+import "components/logo-marquee.js";
